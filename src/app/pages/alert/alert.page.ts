a0ca841b1c88be0dbfe76ad47937a7d09112d27c
David Forero <davidforero717@gmail.com>
2022-02-07 08:42:56 -0500

    se utiliza ion-alert, mostrando alerta con mensaje y con input

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  titulo:string;
 
-  ngOnInit() {
+  constructor(private alertController: AlertController) {}
+
+  ngOnInit() {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Boton Cancelar');
+          },
+        },
+
+        {
+          text: 'Ok',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Boton Ok');
+          },
+        },
+      ],
+    });
+
+    await alert.present();
   }
 
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      header: 'Ingrese su nombre!',
+      inputs: [
+        {
+          name: 'name1',
+          type: 'text',
+          placeholder: 'Nombre'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (data) => {
+            console.log('Confirm Ok', data);
+            this.titulo = data.name1
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
