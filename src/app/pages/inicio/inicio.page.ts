128694f25c86687dfc995a1532dcc7f7c41be76d
David Forero <davidforero717@gmail.com>
2022-02-07 14:46:47 -0500

    se utiliza el ion-checkbox

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -35,6 +35,11 @@ export class InicioPage implements OnInit {
       icon: 'card',
       name: 'Cards',
       redirecTo: '/card'
+    },
+    {
+      icon: 'checkmark-circle-outline',
+      name: 'Checkbox',
+      redirecTo: '/check'
     }
   ]
 
